15e6c09cda63a13d5d3d8bc16bf18fb0f5fae7ae
mcfuddy2 <calvinglennburgess@gmail.com>
2024-10-01 14:22:17 -0600

    We have a balck window that can be closed!

--- main.py
+++ main.py
@@ -1,7 +1,25 @@
 import pygame
+from constants import *
 
 def main():
+    pygame.init
     print("Starting asteroids!")
+    print(f"Screen width: {SCREEN_WIDTH}")
+    print(f"Screen height: {SCREEN_HEIGHT}")
+
+    screen =  pygame.display.set_mode(size=(SCREEN_WIDTH, SCREEN_HEIGHT))
+    
+    loop = True
+    while loop == True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+
+        screen.fill(color=(0, 0, 0))
+        pygame.display.flip()
+        
+    
+    
     return
 
 
