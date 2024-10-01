21db7df276b5f0a524ac839b04ee20682190ae10
mcfuddy2 <calvinglennburgess@gmail.com>
2024-10-01 14:34:29 -0600

    set to 60FPS

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
+import os
+
+os.environ['PYGAME_HIDE_SUPPORT_PROMPT'] = '1'
+
 import pygame
 from constants import *
+
+
 
 def main():
     pygame.init
@@ -8,15 +14,20 @@ def main():
     print(f"Screen height: {SCREEN_HEIGHT}")
 
     screen =  pygame.display.set_mode(size=(SCREEN_WIDTH, SCREEN_HEIGHT))
-    
+    clock = pygame.time.Clock()
+    dt = 0
+
     loop = True
     while loop == True:
+        # this makes the games X button actually close the window
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
-                return
+                loop = False
 
         screen.fill(color=(0, 0, 0))
         pygame.display.flip()
+
+        dt = clock.tick(60) / 1000
         
     
     
